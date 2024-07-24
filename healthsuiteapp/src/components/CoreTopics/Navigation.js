b4976eaf6bc917ab6ab91c534edce6f156eb3105
yhudeeme <josephorji03@gmail.com>
2024-07-24 02:52:50 -0300

    home link added to the library navigation bar

--- healthsuiteapp/src/components/CoreTopics/Navigation.js
+++ healthsuiteapp/src/components/CoreTopics/Navigation.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './Navigation.css';
 import { FaBars } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 function Navigation({ topics, onTopicChange, mobileOpen, setMobileOpen }) {
+
+    const navigate = useNavigate();
+
+    const home = () => {
+        navigate(`/`); 
+    };
+
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
     };
@@ -20,6 +28,11 @@ function Navigation({ topics, onTopicChange, mobileOpen, setMobileOpen }) {
             <nav id="coreSidebar" className={`core-sidebar ${mobileOpen ? 'active' : ''}`}>
                 <div className="core-sidebar-sticky">
                     <ul className="nav flex-column">
+                        <li className="nav-item">
+                                <span className="nav-link" onClick={home}>
+                                    Home
+                                </span>
+                        </li>
                         {topics.map((topic, index) => (
                             <li className="nav-item" key={index}>
                                 <span className="nav-link" onClick={() => handleTopicClick(index)}>
