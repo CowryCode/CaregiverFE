16bbbcdbdc74df5292ed0caba70d8c94e732880a
yhudeeme <josephorji03@gmail.com>
2024-09-10 16:16:41 -0300

    Core topic 5 bookmark completed

--- healthsuiteapp/src/components/CoreTopics/CoreTopics5/Level2/StartingPage.js
+++ healthsuiteapp/src/components/CoreTopics/CoreTopics5/Level2/StartingPage.js
@@ -1,10 +1,15 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useRef, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Typography, IconButton} from '@mui/material';
 import UpdateLibraryLastPage from '../../../../apicall/UpdateLibraryLastPage';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark'; 
+import Tooltip from '@mui/material/Tooltip';
 
 const StartingPage = () => {
     const navigate = useNavigate();
+    const pageTitleRef = useRef(null);
+    const [isBookmarked, setIsBookmarked] = useState(false);
 
     const handleNext = () => {
         navigate('/library/core-topic5/level2/level2one/page1'); // Adjust this path based on your routing structure
@@ -16,15 +21,29 @@ const StartingPage = () => {
 
     useEffect(() => {
         savePageUrl();
-        console.log(`Paged saved successfully : ${successful}` )
-      }, []);
+        setIsBookmarked(bookmarkedAlready);
+      }, [isBookmarked]);
     
-    const { successful, savePageUrl } = UpdateLibraryLastPage({
+    const { successful, savePageUrl,bookmarkPageUrl, bookmarkedAlready } = UpdateLibraryLastPage({
           setLoading: (loading) => console.log(`Loading: ${loading}`),
           handleLibraryClick: (data) => {
               console.log('Library Clicked:', data);
           },
     });
+
+    function handleBookmark(){
+        if(isBookmarked){
+            console.log('This page have been bookmarked already.');
+            alert("This page have been bookmarked already.");
+        }else{
+            if (pageTitleRef.current) {
+                const pageTitle = pageTitleRef.current.textContent.trim();
+                bookmarkPageUrl(pageTitle);
+                setIsBookmarked(true);
+                alert("Bookmarked successfully.");
+            }
+        }
+    }
 
 
     return (
@@ -35,7 +54,14 @@ const StartingPage = () => {
             minHeight: '100vh',
             justifyContent: 'space-between'
         }}>
-            <Typography variant="h4" sx={{ mb: 1, textAlign: 'center' }}>Level 2: Time Management Strategies</Typography>
+            <Box sx={{ display: "flex", justifyContent: "right", alignItems: "right" }}>
+                <Tooltip title="Click to Bookmark">
+                    <IconButton onClick={handleBookmark} aria-label="add to wishlist">
+                        {isBookmarked ? <BookmarkIcon /> : <BookmarkBorderIcon />}
+                    </IconButton>
+                </Tooltip>
+            </Box>
+            <Typography ref={pageTitleRef} variant="h4" sx={{ mb: 1, textAlign: 'center' }}>Level 2: Time Management Strategies</Typography>
             <Typography variant="h6" sx={{ mb: 1, textAlign: 'center', backgroundColor: 'green', color: 'white' }}>Time Management Strategies</Typography>
             <Typography variant="h6" sx={{ mb: 2 }}>Page 1 of 1 (CONTENT)</Typography>
             <ul>
